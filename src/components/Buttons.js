882481d6d4f1f2e2faeec8468e73ede645f3f4b5
Laura <laura.carballo14@gmail.com>
2021-02-25 20:36:16 +1100

    Updated Project Intro

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,14 +2,14 @@ import styled from "styled-components";
 import { typeScale } from "../utils";
 
 const Button = styled.button`
-  padding: 15px 30px;
+  padding: 20px 35px;
   font-size: ${typeScale.helperText};
   font-weight: 700;
   min-width: 100px;
   cursor: pointer;
   font-family: ${(props) => props.theme.primaryFont};
   transition: all 0.5s;
-  border-radius: 25px;
+  border-radius: 30px;
 
   &:hover {
     background-color: ${(props) => props.theme.primaryHoverColor};
@@ -36,9 +36,22 @@ export const PrimaryButton = styled(Button)`
 `;
 
 export const SecondaryButton = styled(Button)`
+  position: relative;
   background-color: ${(props) => props.theme.secondaryButton};
+  padding: 20px 25px 20px 70px;
   border: none;
   color: ${(props) => props.theme.primaryColor};
+
+  &:before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-image: url("images/icon-bookmark.svg");
+    background-repeat: no-repeat;
+    content: "";
+    height: 56px;
+    width: 56px;
+  }
 `;
 
 export const TertiaryButton = styled(Button)`
